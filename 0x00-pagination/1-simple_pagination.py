b80f60610290a139efd49b74955033908745ea49
davykoch <koechdavis24@gmail.com>
2024-06-29 00:13:06 +0300

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""This module contains a Server class to paginate a database
+of popular baby names."""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Calculate start and end indices for a given page and page_size.
+
+    Args:
+    - page (int): The page number (1-indexed).
+    - page_size (int): The number of items per page.
+
+    Returns:
+    - tuple: A tuple containing the start index and end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Retrieve a page of data from the dataset.
+
+        Args:
+        - page (int): The page number (1-indexed). Defaults to 1.
+        - page_size (int): The number of items per page. Defaults to 10.
+
+        Returns:
+        - List[List]: A list of rows for the specified page.
+        """
+        assert isinstance(page, int) and page > 0, (
+            "Page must be a positive integer")
+        assert isinstance(page_size, int) and page_size > 0, (
+            "Page size must be a positive integer")
+
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
