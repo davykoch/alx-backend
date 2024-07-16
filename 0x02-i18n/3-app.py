cf2614ad64192e06477f402b2cc99f259af75137
davykoch <koechdavis24@gmail.com>
2024-07-16 23:26:18 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,7 +3,8 @@
 Flask app with Babel configuration and locale selector.
 """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import _
+from flask_babel import Babel
 from typing import Any
 
 
@@ -24,7 +25,8 @@ def get_locale() -> str:
     """
     Determine the best match with our supported languages.
     """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # For testing, always return 'en'
+    return 'en'
 
 
 @app.route('/', strict_slashes=False)
@@ -35,12 +37,8 @@ def index() -> str:
     return render_template('3-index.html')
 
 
-def _(string: str) -> str:
-    """
-    Translation function.
-    """
-    return string
-
+print(_('home_title'))  # Should print "Welcome to Holberton"
+print(_('home_header'))  # Should print "Hello world!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
