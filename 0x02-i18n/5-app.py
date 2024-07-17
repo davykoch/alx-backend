c438d976f6cd9b933a8d19bc970b52c93bfd777b
davykoch <koechdavis24@gmail.com>
2024-07-17 23:01:57 +0300

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -16,13 +16,16 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 class Config:
     """Configuration class for Flask app."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 def get_user() -> Union[Dict, None]:
     """
@@ -34,12 +37,14 @@ def get_user() -> Union[Dict, None]:
         return users.get(int(login_id))
     return None
 
+
 @app.before_request
 def before_request():
     """
     Use get_user to find a user if any, and set it as a global on flask.g.user.
     """
     g.user = get_user()
+
 
 @babel.localeselector
 def get_locale() -> str:
@@ -51,12 +56,14 @@ def get_locale() -> str:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
     Render the index page with a title and header.
     """
     return render_template('5-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
